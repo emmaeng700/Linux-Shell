ece6985c7301d409fb69c7f5694731bc83e8bf01
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-20 20:58:43 -0600

    Function Declarations for builtin shell commands

--- main.c
+++ main.c
@@ -45,3 +45,30 @@ HANDLE custom_fork(const char* cmd) {
     CloseHandle(pi.hThread); 
     return pi.hProcess;
 }
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
