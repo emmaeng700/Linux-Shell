6d94f3e640a68baba351c9d67ac057237694b30b
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-20 21:02:10 -0600

    working on Reading a line in shell

--- main.c
+++ main.c
@@ -190,3 +190,74 @@ char **lsh_split_line(char *line) {
     return tokens;
 } 
 
+#define LSH_RL_BUFSIZE 1024
+
+// Reading a line
+char *lsh_read_line(void) {
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // read a character
+        c = getchar();
+
+        // if we hit EOF, replace it with a null character and return
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position ++;
+
+        // if we have exceeded the buffer, reallocate.
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error \n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void lsh_loop(void) {
+    /*
+    Shell can start via main(). What shell does during loop (lsh_loop()):
+        1. Read: read cmd input
+        2. Parse: separate cmd string into a program and arguments
+        3. run the parsed cmd
+    */
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv) {
+    // load config files
+    //run command loop
+    lsh_loop();
+
+    // perform any cleanup or shutdown
+
+    return EXIT_SUCCESS;
+}
